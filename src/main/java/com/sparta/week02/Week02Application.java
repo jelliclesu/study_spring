b53fc6d3da1fc797510a1d0f0e6a8dad0a84e7e5
ssufunny <wltnwhgdkgo@naver.com>
2024-07-17 21:57:28 +0900

    Dto 적용

--- src/main/java/com/sparta/week02/Week02Application.java
+++ src/main/java/com/sparta/week02/Week02Application.java
@@ -2,6 +2,7 @@ package com.sparta.week02;
 
 import com.sparta.week02.domain.Course;
 import com.sparta.week02.domain.CourseRepository;
+import com.sparta.week02.domain.CourseRequestDto;
 import com.sparta.week02.service.CourseService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -18,40 +19,32 @@ public class Week02Application {
     public static void main(String[] args) {
         SpringApplication.run(Week02Application.class, args);
     }
+
     @Bean
-    public CommandLineRunner demo(CourseRepository repository, CourseService courseService, CourseRepository courseRepository) {
+    public CommandLineRunner demo(CourseRepository courseRepository, CourseService courseService) {
         return (args) -> {
-            // 데이터 저장하기(Create)
-            repository.save(new Course("Spring", "곽두필"));
+            courseRepository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
 
-            // 데이터 전부 조회하기(Read)
-            List<Course> courseList = repository.findAll();
-            for (int i=0; i < courseList.size(); i++) {
+            System.out.println("데이터 인쇄");
+            List<Course> courseList = courseRepository.findAll();
+            for (int i=0; i<courseList.size(); i++) {
                 Course course = courseList.get(i);
                 System.out.println(course.getId());
                 System.out.println(course.getTitle());
                 System.out.println(course.getTutor());
             }
 
-            // 데이터 하나 조회하기(Read)
-//            Course course = repository.findById(1L).orElseThrow(
-//                    () -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다.")
-//            );
-
-            // Update
-            Course new_course = new Course("Spring", "박두식");
-            courseService.update(1L, new_course);
+            CourseRequestDto requestDto = new CourseRequestDto("웹개발의 봄, Spring", "임민영");
+            courseService.update(1L, requestDto);
             courseList = courseRepository.findAll();
-            for (int i=0; i < courseList.size(); i++) {
+            for (int i=0; i<courseList.size(); i++) {
                 Course course = courseList.get(i);
                 System.out.println(course.getId());
                 System.out.println(course.getTitle());
                 System.out.println(course.getTutor());
             }
 
-            // Delete
             courseRepository.deleteAll();
         };
     }
-
 }
--- src/main/java/com/sparta/week02/domain/Course.java
+++ src/main/java/com/sparta/week02/domain/Course.java
@@ -1,42 +1,33 @@
 package com.sparta.week02.domain;
 
+import com.sparta.week02.domain.CourseRequestDto;
+import com.sparta.week02.domain.Timestamped;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor // 기본생성자 대신 생성
-@Entity // 테이블임을 나타냄
 @Getter
-public class Course extends Timestamped{
+@NoArgsConstructor
+@Entity
+public class Course extends Timestamped {
 
-    @Id // ID 값, Primary Key로 사용하겠다는 뜻
-    @GeneratedValue(strategy = GenerationType.AUTO) // 자동 증가 명령
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(nullable = false) // 컬럼 값이고 반드시 값이 존재해야 함
+    @Column(nullable = false)
     private String title;
 
     @Column(nullable = false)
     private String tutor;
-
-//    public Long getId() {
-//        return this.id;
-//    }
-//    public String getTitle() {
-//        return this.title;
-//    }
-//
-//    public String getTutor() {
-//        return this.tutor;
-//    }
 
     public Course(String title, String tutor) {
         this.title = title;
         this.tutor = tutor;
     }
 
-    public void update(Course course) {
-        this.title = course.title;
-        this.tutor = course.tutor;;
+    public void update(CourseRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.tutor = requestDto.getTutor();
     }
 }
