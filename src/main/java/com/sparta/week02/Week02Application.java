b42e089524a1eb9c8efc84a9701d86423004cfa4
ssufunny <wltnwhgdkgo@naver.com>
2024-07-17 20:11:52 +0900

    JPA 사용해보기

--- src/main/java/com/sparta/week02/Week02Application.java
+++ src/main/java/com/sparta/week02/Week02Application.java
@@ -1,13 +1,22 @@
 package com.sparta.week02;
 
+import com.sparta.week02.domain.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Week02Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Week02Application.class, args);
+    }
+    @Bean
+    public CommandLineRunner demo(CourseRepository repository) {
+        return (args) -> {
+
+        };
     }
 
 }
