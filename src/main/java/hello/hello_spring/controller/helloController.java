6350180264d57a66eea382568b5e1f6cf3ca2cc0
jelliclesu <jelliclesu@naver.com>
2024-09-04 16:36:43 +0900

    @ResponseBody 문자 반환 예시

--- src/main/java/hello/hello_spring/controller/helloController.java
+++ src/main/java/hello/hello_spring/controller/helloController.java
@@ -4,7 +4,9 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class helloController {
@@ -20,4 +22,9 @@ public class helloController {
         return "hello-template";
     }
 
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
 }
