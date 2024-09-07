33e6ae25003bd8f9d68c7171dbd2e5c38be9d924
jelliclesu <jelliclesu@naver.com>
2024-09-07 19:52:57 +0900

    [회원 관리 예제] feat: MemberService 생성 (회원 가입, 중복 회원 검증, 전체 회원 조회 기능)

---
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,40 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member);    // 같은 이름이 있는 중복 회원X
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");  // 값이 있으면 로직이 동작 - Optional이라 가능
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
