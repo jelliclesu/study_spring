6b8c54d857fcb6675b19e6ab85f4247c7a63dd9c
jelliclesu <jelliclesu@naver.com>
2024-09-11 19:44:34 +0900

    [회원 관리 예제] feat: MemberService 변경 - 회원가입, 회원조회 시간 측정 추가

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -29,9 +29,18 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
-        validateDuplicateMember(member);    // 같은 이름이 있는 중복 회원X
-        memberRepository.save(member);
-        return member.getId();
+
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member);    // 같은 이름이 있는 중복 회원X
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join= " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -45,7 +54,16 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+
+        long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
