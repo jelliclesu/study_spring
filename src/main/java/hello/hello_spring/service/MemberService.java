8665018fed332822de63102a41e141bb8e419d5b
jelliclesu <jelliclesu@naver.com>
2024-09-11 19:50:04 +0900

    [회원 관리 예제] feat: TimeTraceAop 생성 - 시간 측정 AOP 등록

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -29,18 +29,9 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
-
-        long start = System.currentTimeMillis();
-
-        try {
-            validateDuplicateMember(member);    // 같은 이름이 있는 중복 회원X
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join= " + timeMs + "ms");
-        }
+        validateDuplicateMember(member);    // 같은 이름이 있는 중복 회원X
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -54,16 +45,7 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-
-        long start = System.currentTimeMillis();
-
-        try {
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers = " + timeMs + "ms");
-        }
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
