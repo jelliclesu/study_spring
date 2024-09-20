67df1db6cbeb962509bf79f7f13e961fc1fe06ba
jelliclesu <jelliclesu@naver.com>
2024-09-20 20:15:36 +0900

    [주문/할인 정책] test: MemberServiceTest.java, OrderServiceTest.java 수정 - AppConfig 적용하여 테스트 오류 수정

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,17 +1,25 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderSerive orderSerive = new OrderServiceImpl();
+    MemberService memberService;
+    OrderSerive orderSerive;
 
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderSerive = appConfig.orderSerive();
+    }
     @Test
     void createOrder() {
         Long memberId = 1L;
