7c78e913620075a2e102725ca1fcb8dfef8e60fa
jelliclesu <jelliclesu@naver.com>
2024-09-20 19:41:41 +0900

    [주문/할인 정책] feat: OrderApp.java 생성 - 주문과 할인 정책 실행

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderSerive;
+import hello.core.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderSerive orderSerive = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member  = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order  = orderSerive.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+    }
+}
