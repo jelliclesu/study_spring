7140c23f4eec5c5184d80af2dd5f18799bc7b3f0
jelliclesu <jelliclesu@naver.com>
2024-09-24 19:31:26 +0900

    [주문/할인 정책] test: SingletonTest.java 수정 - 싱글톤 패턴 테스트 추가

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -30,4 +30,26 @@ public class SingletonTest {
         // memberService1 != memberService2
         assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+
+        // private 로 생성자를 막아둬서 컴파일 에러 생성
+        // new SingletonService();
+
+        // 1. 조회: 호출 할 때마다 같은 객체를 반환
+        SingletonService singletonService1 = SingletonService.getInstance();
+        // 2. 조회: 호출 할 때마다 같은 객체를 반환
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        // 참조값이 같은 것 확인
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        // singletonService1 == singletonService2
+        assertThat(singletonService1).isSameAs(singletonService2);
+
+    }
 }
