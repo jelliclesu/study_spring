810b6771b013a4a164161a7a133795532da970a7
jelliclesu <jelliclesu@naver.com>
2024-09-25 21:04:34 +0900

    [주문/할인 정책] test: NetworkClient.java 수정 - 인터페이스 추가

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
@@ -26,5 +29,20 @@ public class NetworkClient {
     // 서비스 종료 시 호출
     private void disconnect() {
         System.out.println("close = " + url);
+    }
+
+    // 의존 관계 주입이 끝나면
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 빈이 종료될 때
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
