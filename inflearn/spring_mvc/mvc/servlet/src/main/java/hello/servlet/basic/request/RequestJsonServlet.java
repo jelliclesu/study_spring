20f1ed3049f585c5420f395eff8331a93a8394e5
jelliclesu <jelliclesu@naver.com>
2026-01-15 22:00:11 +0900

    [MVC_Servlet] fix: gradle 버전 및 import 오류 해결

--- inflearn/spring_mvc/mvc/servlet/src/main/java/hello/servlet/basic/request/RequestJsonServlet.java
+++ inflearn/spring_mvc/mvc/servlet/src/main/java/hello/servlet/basic/request/RequestJsonServlet.java
@@ -1,6 +1,5 @@
 package hello.servlet.basic.request;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import hello.servlet.basic.HelloData;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletInputStream;
@@ -9,6 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.util.StreamUtils;
+import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
--- inflearn/spring_mvc/mvc/servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
+++ inflearn/spring_mvc/mvc/servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -1,12 +1,12 @@
 package hello.servlet.basic.response;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import hello.servlet.basic.HelloData;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
