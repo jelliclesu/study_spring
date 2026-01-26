a5d6591c6b1aef67713173fb5433be062ed047b0
jelliclesu <jelliclesu@naver.com>
2026-01-26 18:53:46 +0900

    [MVC] 타임리프 - 상품 수정 폼 (섹션 8)

--- inflearn/spring_mvc/mvc/item-service/src/main/java/hello/item_service/web/item/basic/BasicItemController.java
+++ inflearn/spring_mvc/mvc/item-service/src/main/java/hello/item_service/web/item/basic/BasicItemController.java
@@ -73,6 +73,20 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId,
+                           Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId,
+                           Model model) {
+        return "basic/editForm";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
