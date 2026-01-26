e5b472183312eaf1ed1148728eec43aa95d733f3
jelliclesu <jelliclesu@naver.com>
2026-01-26 19:08:58 +0900

    [MVC] PRG 적용 (섹션 8)
    - 새로고침 문제 해결

--- inflearn/spring_mvc/mvc/item-service/src/main/java/hello/item_service/web/item/basic/BasicItemController.java
+++ inflearn/spring_mvc/mvc/item-service/src/main/java/hello/item_service/web/item/basic/BasicItemController.java
@@ -67,10 +67,19 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(Item item) {
         itemRepository.save(item);
         return "basic/item";
+    }
+
+    /**
+     * PRG: POST - Redirect - Get
+     */
+    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
     }
 
     @GetMapping("/{itemId}/edit")
